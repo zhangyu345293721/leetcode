be533ea29df14e2f76d24948f9f241743711f935
张瑀 <zhangyu_xtb@geekplus.cc>
2019-07-25 10:05:15 +0800

    翻转一个integer的数

---
+++ src/leetcode/python/reverse_integer.py
@@ -0,0 +1,25 @@
+# encoding='utf-8'
+
+'''
+    author:zhangyu
+    date:2019.7.25
+    description 翻转一个数字
+'''
+
+def get_reverse_integer(num):
+    if num == 0:
+        return num
+    sum = 0
+    while num != 0:
+        sum = 10 * sum + num % 10
+        # python3中整除应该是//这样比较准确
+        num = num // 10
+    if int(sum) == sum:
+        return sum
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    number = get_reverse_integer(123459999999999999999)
+    print(number)
