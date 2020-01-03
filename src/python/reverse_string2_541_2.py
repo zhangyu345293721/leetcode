c12ebc17bd9db1fcef54ee06e11e97138a2ba224
octopus <345293721@qq.com>
2020-01-03 09:51:43 +0800

    Update reverse_string2_541_2.py

--- src/python/reverse_string2_541_2.py
+++ src/python/reverse_string2_541_2.py
@@ -5,7 +5,7 @@
    description: 按照要求反转一个字符串2
    date:2019.8.19
 '''
-
+import re
 
 def get_reverse_string(s, k):
         list = []
@@ -46,7 +46,22 @@ def get_reverse_string2(s, k):
                 sb += list[i]
         return sb
     return '-1'
+   
+# 截取固定长度的字符串
+def cut_text(text, lenth):
+    textArr = re.findall('.{' + str(lenth) + '}', text)
+    textArr.append(text[(len(textArr) * lenth):])
+    return textArr
 
+
+def get_reverse_str(arr):
+    s = ''
+    for i in range(len(arr)):
+        if i % 2 == 0:
+            s += arr[i][::-1]
+        else:
+            s += arr[i]
+    return s
 if __name__ == '__main__':
     s = 'abc'
     revese_s = get_reverse_string(s, 2)
