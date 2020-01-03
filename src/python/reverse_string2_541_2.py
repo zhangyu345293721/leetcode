db1c17b3f1208831b850bd5dd7cf67903611fb02
octopus <345293721@qq.com>
2020-01-03 09:50:23 +0800

    Update reverse_string2_541_2.py

--- src/python/reverse_string2_541_2.py
+++ src/python/reverse_string2_541_2.py
@@ -21,6 +21,31 @@ def get_reverse_string(s, k):
             else:
                 sb += list[i]
         return sb
+ 
+def get_reverse_string2(s, k):
+    # 当s字符串长度小于k的值
+    if len(s) <= k:
+        return s[::-1]
+    # 当字符串长度在k和2k之间
+    if len(s) > k and len(s) <= 2 * k:
+        temp_str = s[0: k]
+        return temp_str[::-1] + s[k:len(s)]
+    # 当字符串长度大于2*k
+    if len(s) > 2 * k:
+        list = []
+        sb = ''
+        number = len(s) // k
+        for i in range(number):
+            list.append(s[i * k:i * k + k])
+        if number * k < len(s):
+            list.append(s[number * k:len(s)])
+        for i in range(len(list)):
+            if i % 2 == 0:
+                sb += list[i][::-1]
+            else:
+                sb += list[i]
+        return sb
+    return '-1'
 
 if __name__ == '__main__':
     s = 'abc'
