ccdcac6c2afcec51cd3d679f883a5e415a5ba582
张瑀 <zhangyu_xtb@geekplus.cc>
2020-04-16 23:53:49 +0800

    master:增加题目56注释

--- src/leetcodejava/array/MergeIntervals56.java
+++ src/leetcodejava/array/MergeIntervals56.java
@@ -4,28 +4,27 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 /**
  * This is the solution of No. 56 problem in the LeetCode,
  * the website of the problem is as follow:
  * https://leetcode-cn.com/problems/merge-intervals
- *
+ * <p>
  * The description of problem is as follow:
  * ==========================================================================================================
  * 给出一个区间的集合，请合并所有重叠的区间。
- *
+ * <p>
  * 示例 1:
  * 输入: [[1,3],[2,6],[8,10],[15,18]]
  * 输出: [[1,6],[8,10],[15,18]]
  * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
  * 示例 2:
- *
+ * <p>
  * 输入: [[1,4],[4,5]]
  * 输出: [[1,5]]
  * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * ==========================================================================================================
  *
@@ -36,10 +35,9 @@ public class MergeIntervals56 {
 
     @Test
     public void testMergeIntervals() {
-        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
-        int[][] arr = merge(intervals);
-        System.out.println(arr);
-        System.out.println("hhhh:  "+new Date());
+        int[][] intervals = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
+        int[][] result = merge(intervals);
+        System.out.println(result);
     }
 
     /**
@@ -63,6 +61,7 @@ public class MergeIntervals56 {
                 curInterval = intervals[i];
             }
         }
+        // 要加上临时保存的区间
         list.add(curInterval);
         int[][] ret = new int[list.size()][];
         for (int i = 0; i < list.size(); i++) {
