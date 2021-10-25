c0c03196ddb8cc2321ee1f33e0ceb40f013712eb
张瑀 <zhangyu_xtb@geekplus.cc>
2021-10-25 09:39:44 +0800

    master:修改题目注释

--- src/leetcodejava/array/EightQueens0812.java
+++ src/leetcodejava/array/EightQueens0812.java
@@ -1,5 +1,62 @@
-class Solution {
+package leetcodejava.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is the solution of No.0812 problem in the LeetCode,
+ * the website of the problem is as follow:
+ * https://leetcode-cn.com/problems/eight-queens-lcci
+ * <p>
+ * The description of problem is as follow:
+ * ==========================================================================================================
+ * 设计一种算法，打印 N 皇后在 N × N 棋盘上的各种摆法，其中每个皇后都不同行、不同列，也不在对角线上。这里的“对角线”指的是所有的对角线，不只是平分整个棋盘的那两条对角线。
+ *
+ * 注意：本题相对原题做了扩展
+ *
+ * 示例:
+ *
+ *  输入：4
+ *  输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ *  解释: 4 皇后问题存在如下两个不同的解法。
+ * [
+ *  [".Q..",  // 解法 1
+ *   "...Q",
+ *   "Q...",
+ *   "..Q."],
+ *
+ *  ["..Q.",  // 解法 2
+ *   "Q...",
+ *   "...Q",
+ *   ".Q.."]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/eight-queens-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * ==========================================================================================================
+ *
+ * @author zhangyu (zhangyuyu417@gmail.com)
+ */
+public class EightQueens0812 {
+    @Test
+    public void EightQueensTest() {
+        int n = 4;
+        List<List<String>> result = solveNQueens(n);
+        Assert.assertEquals(result.size(), 2);
+    }
+
     private List<List<String>> result = new ArrayList();
+
+    /**
+     * 八皇后问题
+     *
+     * @param n 八皇后数
+     * @return 结果链表
+     */
     public List<List<String>> solveNQueens(int n) {
         char[][] board = new char[n][n];
         for (int i = 0; i < n; ++i) {
@@ -8,10 +65,11 @@ class Solution {
             }
         }
         backtrack(0, board, n);
-        return result; 
+        return result;
     }
+
     /**
-     * 递归回溯求解    
+     * 递归回溯求解
      */
     private void backtrack(int row, char[][] board, int n) {
         // 结束条件，得到可⾏解
@@ -31,6 +89,7 @@ class Solution {
             }
         }
     }
+
     /**
      * 检查该位置放皇后是否合法
      */
