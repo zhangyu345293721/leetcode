ec071f6e8a3b397ce65532e4c8e0aa46ad1acc1e
octopus <345293721@qq.com>
2023-05-30 23:04:19 +0800

    Update SearchRotatedSortedArray33.java

--- src/leetcodejava/array/SearchRotatedSortedArray33.java
+++ src/leetcodejava/array/SearchRotatedSortedArray33.java
@@ -97,4 +97,40 @@ public class SearchRotatedSortedArray33 {
         }
         return -1;
     }
+    
+    
+     /**
+     * 找到目标数字的下标
+     *
+     * @param nums   数组
+     * @param target 目标值
+     * @return 下标
+     */
+    public int search3(int[] nums, int target) {
+        if(nums == null || nums.length < 1) {
+            return -1;
+        }
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right) {
+            // 防止数字溢出
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < nums[right]) {
+                if(nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                if(nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
 }
