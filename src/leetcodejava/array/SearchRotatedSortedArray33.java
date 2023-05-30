a5b0d394cea248291080129d4b6694da5cfc73f2
octopus <345293721@qq.com>
2023-05-30 22:37:32 +0800

    Update SearchRotatedSortedArray33.java

--- src/leetcodejava/array/SearchRotatedSortedArray33.java
+++ src/leetcodejava/array/SearchRotatedSortedArray33.java
@@ -73,9 +73,9 @@ public class SearchRotatedSortedArray33 {
         if (nums == null || nums.length < 1) {
             return -1;
         }
-        int low = 0, high = nums.length - 1, mid = 0;
+        int low = 0, high = nums.length - 1;
         while (low <= high) {
-            mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
