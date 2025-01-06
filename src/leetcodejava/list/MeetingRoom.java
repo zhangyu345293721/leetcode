6df1a13c0f09047ff654818a643766afb3ee2a65
octopus <345293721@qq.com>
2025-01-06 17:22:19 +0800

    Update MeetingRoom.java

--- src/leetcodejava/list/MeetingRoom.java
+++ src/leetcodejava/list/MeetingRoom.java
@@ -16,88 +16,38 @@ import java.util.Arrays;
 public class AddTwoNumbers2 {
     @Test
     public void addTwoNumbers() {
-        ListNode node1 = ListNode.createListNode(Arrays.asList(9, 9, 9, 9, 9, 9, 9));
-        ListNode node2 = ListNode.createListNode(Arrays.asList(9, 9, 9, 9));
-        ListNode result = addTwoNumbers(node1, node2);
+        int[][] intervals = {{0, 30}, {5, 10}, {15, 20}};
+        int result = minMeetingRooms(intervals);
+        System.out.println("最少会议室数量: " +result);
         System.out.println(result);
-        Assert.assertEquals(result.val, 8);
+        Assert.assertEquals(result, 2);
     }
-
     
     /**
      * 两个链表相加
      *
-     * @param l1 链表1
-     * @param l2 链表2
-     * @return 链表结果
+     * @param intervals interval array
+     * @return 结果
      */
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
+     public static int minMeetingRooms(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
         }
-        if (l2 == null) {
-            return l1;
-        }
-        ListNode dummy = new ListNode(-1);
-        ListNode p = dummy;
-        int carry = 0;
-        while (l1 != null || l2 != null) {
-            int num1 = l1 == null ? 0 : l1.val;
-            int num2 = l2 == null ? 0 : l2.val;
-            int total = num1 + num2 + carry;
-            carry = total / 10;
-            p.next = new ListNode(total % 10);
-            if (l1 != null) {
-                l1 = l1.next;
+        // 按照会议的开始时间排序
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        // 最小堆，按会议的结束时间排序
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        // 添加第一个会议的结束时间
+        minHeap.add(intervals[0][1]);
+        for (int i = 1; i < intervals.length; i++) {
+            // 如果当前会议的开始时间 >= 堆顶的结束时间，弹出堆顶
+            if (intervals[i][0] >= minHeap.peek()) {
+                minHeap.poll();
             }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
-            p = p.next;
+            // 添加当前会议的结束时间
+            minHeap.add(intervals[i][1]);
         }
-        if (carry > 0) {
-            p.next = new ListNode(carry);
-        }
-        return dummy.next;
-    }
-
-    /**
-     * 两个链表相加
-     *
-     * @param l1 链表1
-     * @param l2 链表2
-     * @return 链表结果
-     */
-    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        return this.addTwoNumberList(l1, l2, 0);
-    }
-
-    /**
-     * 两个链表相加
-     *
-     * @param l1 链表1
-     * @param l2 链表2
-     * @param v  数字v
-     * @return 返回合并后链表
-     */
-    private ListNode addTwoNumberList(ListNode l1, ListNode l2, int v) {
-        if (l1 == null && l2 == null) {
-            return v == 0 ? null : new ListNode(v);
-        }
-        if (l1 != null) {
-            v += l1.val;
-            l1 = l1.next;
-        }
-        if (l2 != null) {
-            v += l2.val;
-            l2 = l2.next;
-        }
-        return new ListNode(v % 10, addTwoNumberList(l1, l2, v / 10));
+        // 堆的大小就是所需的最少会议室数
+        return minHeap.size();
     }
 }
